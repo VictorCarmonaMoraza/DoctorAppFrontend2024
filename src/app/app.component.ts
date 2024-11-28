0b61922db52a300d744207428a9e331a97f35ecc
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-11-28 20:20:15 +0100

    Mostrar usuarios en la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DoctorApp2024';
+  usuarios: any;
+
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5014/api/User').subscribe({
+      next: response => this.usuarios = response,
+      error: error => console.log(error),
+      complete: () => console.log('La solicitud esta completa')
+    })
+  }
 }
